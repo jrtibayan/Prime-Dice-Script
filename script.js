065a6ecfd0196bca812ebe08e06d144cadecfe75
JericSheila <jrhod_baby@yahoo.com>
2017-06-28 16:42:14 +0800

    Buttons and Inputs in Primedice Website

---
+++ script.js
@@ -0,0 +1,13 @@
+ele = {
+    btnRoll : document.querySelector(".index__home__dice__wrap__cta.btn"),
+    btnBetInputX2 : document.querySelector(".dice__control__content").children[2],
+    btnBetInputHalf : document.querySelector(".dice__control__content").children[1],
+    btnOnLossReset : document.querySelector(".index__home__dice__automated-switch").children[0].children[0].children[0].children[1].children[0].children[0],
+    btnOnLossIncrease : document.querySelector(".index__home__dice__automated-switch").children[0].children[0].children[0].children[1].children[0].children[1],
+    btnDirection : document.querySelector(".index__home__dice__card__bet-ui").children[1].children[1].children[0].children[0].children[0].children[1].children[0],
+    // OTHERS
+    myBal : document.querySelector(".index__home__header__balance__btc").children[1],
+    lastRollContainer : document.querySelector(".index__home__indicator__inner__number__roll"),
+    lastRollResult : document.querySelector(".index__home__indicator__inner__number__roll").children[1],
+    betAmountInput : document.getElementById("betSize")
+};
