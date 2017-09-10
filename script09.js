db07cb40b4374f69699383f709122bf1c438d6b2
JericSheila <jrhod_baby@yahoo.com>
2017-09-10 16:44:26 +0800

    new pattern being used and better stats

--- script09.js
+++ script09.js
@@ -1,5 +1,13 @@
 /* Bet on SAME after 3 consecutive OPPOSITE */
 
+/*
+
+    test:
+    9/10/2017 bet:256 balance: 30,000 -> 46,736 highestOpposite: 8 highestSame: 4
+
+
+ */
+
 ele = {
     betBtn: document.querySelector(".index__home__dice__wrap__cta.btn"),
     halfBtn: document.querySelector(".dice__control__content").children[1],
@@ -20,22 +28,65 @@ model = {
     currentConsecLoss: null,
     betting: false,
 
-
-
     settings: {
         betClicks: {
             base: 5,
-            dummy: 1,
+            dummy: 0,
             current: null
         },
+        patternZigZag: false,
         targetIncome: 0,
         zigZagCountBeforeBet: 3
     },
 
     stats: {
-        rolls: [] // {direction, rollNumber}
+        rolls: [], // {direction, rollNumber}
+        realBetSameCount: 0,
+        realBetOppositeCount: 0,
+        realBetMissCount: 0,
+        same: {
+            win: 0,
+            loss: 0,
+            lossStreak: 0,
+            winStreak: 0,
+
+            miss: 0,
+            same: 0,
+            opposite: 0,
+
+            missStreak: 0,
+            sameStreak: 0,
+            oppositeStreak: 0,
+
+            highestMiss: 0,
+            highestSame: 0,
+            highestOpposite: 0
+        },
+        zigzag: {
+            win: 0,
+            loss: 0,
+            lossStreak: 0,
+            winStreak: 0,
+
+            miss: 0,
+            same: 0,
+            opposite: 0,
+
+            missStreak: 0,
+            sameStreak: 0,
+            oppositeStreak: 0,
+
+            highestMiss: 0,
+            highestSame: 0,
+            highestOpposite: 0
+        }
     }
 };
+
+wStreakCounter = 0;
+lStreakCounter = 0;
+wStreak = 0;
+lStreak = 0;
 
 helper = {
 
@@ -53,7 +104,15 @@ mv = {
         if(ele.directionSpan.innerText !== lastDirection) {
             ele.directionSpan.click();
         }
-        console.log('last is: '+lastDirection+'\nbetting: ele.directionSpan.innerText');
+        //console.log('last is: '+lastDirection+'\nbetting same:'+ele.directionSpan.innerText);
+    },
+
+    setOppositeDirection: function() {
+        lastDirection = mv.checkDirection(model.stats.rolls[model.stats.rolls.length-1]);
+        if(ele.directionSpan.innerText === lastDirection) {
+            ele.directionSpan.click();
+        }
+        console.log('last is: '+lastDirection+'\nbetting opposite:'+ele.directionSpan.innerText);
     },
 
     setAmountZero: function () {
@@ -68,12 +127,21 @@ mv = {
             ele.x2Btn.click();
         }
     },
+    setDummyClick: function(num) {
+        model.settings.betClicks.dummy = num;
+    },
+
+    setBaseClick: function(num) {
+        model.settings.betClicks.base = num;
+    },
+
     setBetAmount: function () {
         mv.setAmountZero();
         for(a=0;a<model.settings.betClicks.current;a++) {
             ele.x2Btn.click();
         }
     },
+
     increaseActionIndex: function () {
         model.actionIndex++;
     },
@@ -87,48 +155,85 @@ mv = {
     },
 
     recordRoll: function () {
+        // record the roll
         model.stats.rolls.push( parseFloat(ele.lastRollSpan.innerText) );
+
+/*
+        lastRollIndex = model.stats.rolls.length-1;
+        // increase which direction it is in
+        if( mv.checkDirection( model.stats.rolls[lastRollIndex] )==='MISS' ) {
+            model.stats.realBetSameCount++;
+        } else if() {
+
+        }
+*/
+
     },
 
     lastRollsZigZag: function() {
-        okay = true;
-        endIndex = model.stats.rolls.length-1; // 4
-        //console.log('End: '+model.stats.rolls[endIndex]+ ' ' +endIndex);
-        startIndex = endIndex-model.settings.zigZagCountBeforeBet; // 2
-        //console.log('Start: '+model.stats.rolls[startIndex]+ ' ' + startIndex);
-        indexBeforeZigZag = startIndex-1; // 1
-        indexBeforeZigZag2 = startIndex-2; // 1
 
-        if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag] )==='MISS' ) {
-            return false;
-        }
+        if(model.settings.patternZigZag===true) {
+            // 1, 91, 91, 1, 91, 1
+            endIndex = model.stats.rolls.length-1; // 4
+            //console.log('End: '+model.stats.rolls[endIndex]+ ' ' +endIndex);
+            startIndex = endIndex-model.settings.zigZagCountBeforeBet; // 2
+            //console.log('Start: '+model.stats.rolls[startIndex]+ ' ' + startIndex);
+            indexBeforeZigZag = startIndex-1; // 1
+            indexBeforeZigZag2 = startIndex-2; // 1
 
-        if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag2] )==='MISS' ) {
-            return false;
-        }
-
-        if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag])===mv.checkDirection(model.stats.rolls[indexBeforeZigZag2]) ) {
-            return false;
-        }
-
-        if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag] )!==mv.checkDirection(model.stats.rolls[startIndex]) ) {
-            return false;
-        }
-
-        lastDirection = mv.checkDirection(model.stats.rolls[startIndex]);
-        for(a=startIndex+1;a<=endIndex;a++) {
-
-            if(mv.checkDirection(model.stats.rolls[a])==='MISS') {
+            if( mv.checkDirection( model.stats.rolls[indexBeforeZigZag] )==='MISS' ) {
+                //console.log('MISS FOUND1');
                 return false;
             }
 
-            if(lastDirection === mv.checkDirection(model.stats.rolls[a])) {
+            if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag2] )==='MISS' ) {
+                //console.log('MISS FOUND2');
                 return false;
             }
-            else {
+
+            if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag])===mv.checkDirection(model.stats.rolls[indexBeforeZigZag2]) ) {
+                //console.log('index before zigzag === to zigzag2');
+                return false;
+            }
+
+            if( mv.checkDirection(model.stats.rolls[indexBeforeZigZag] )!==mv.checkDirection(model.stats.rolls[startIndex]) ) {
+                //console.log('index before zigzag != to start');
+                return false;
+            }
+
+            lastDirection = mv.checkDirection(model.stats.rolls[startIndex]);
+            for(a=startIndex+1;a<=endIndex;a++) {
+
+                if(mv.checkDirection(model.stats.rolls[a])==='MISS') {
+                    //console.log('MISS FOUND3');
+                    return false;
+                }
+
+                if(lastDirection === mv.checkDirection(model.stats.rolls[a])) {
+                    return false;
+                }
+                else {
+                    lastDirection = mv.checkDirection(model.stats.rolls[a]);
+                }
+            }
+        }
+        else {
+            lastIndex = model.stats.rolls.length-1;
+            startOfSame = lastIndex-4;
+            endOfSame = lastIndex-1 ;
+
+            if( mv.checkDirection(model.stats.rolls[startOfSame] )==='MISS' ) return false;
+            if( mv.checkDirection(model.stats.rolls[lastIndex] )==='MISS' ) return false;
+
+            lastDirection = mv.checkDirection(model.stats.rolls[startOfSame]);
+            for(a=startOfSame+1;a<=endOfSame;a++) {
+                if(mv.checkDirection(model.stats.rolls[a])!==lastDirection) return false;
                 lastDirection = mv.checkDirection(model.stats.rolls[a]);
             }
+
+            if(mv.checkDirection(model.stats.rolls[lastIndex])===mv.checkDirection(model.stats.rolls[endOfSame]) ) return false;
         }
+
         return true;
     },
 
@@ -180,6 +285,74 @@ mv = {
         return (!mv.hasClass(ele.lastRollContainer, 'is-negative'));
     },
 
+    increaseWinCount: function() {
+        if(model.settings.patternZigZag===true) {
+            // zigzag
+            model.stats.zigzag.win++;
+            console.log('win: ' + model.stats.zigzag.win + ' loss: '+model.stats.zigzag.loss);
+        } else {
+            // same
+            model.stats.same.win++;
+            console.log('win: ' + model.stats.same.win + ' loss: '+model.stats.same.loss);
+        }
+    },
+
+    increaseLossCount: function() {
+        if(model.settings.patternZigZag===true) {
+            // zigzag
+            model.stats.zigzag.loss++;
+            console.log('win: ' + model.stats.zigzag.win + ' loss: '+model.stats.zigzag.loss);
+        } else {
+            // same
+            model.stats.same.loss++;
+            console.log('win: ' + model.stats.same.win + ' loss: '+model.stats.same.loss);
+        }
+    },
+
+    increaseDirectionCount: function() {
+
+        if(model.settings.patternZigZag===true) {
+            pattern = 'zigzag';
+        } else {
+            pattern = 'same';
+        }
+
+        lastDirection = mv.checkDirection(model.stats.rolls[model.stats.rolls.length-1]);
+        directionBeforeLast = mv.checkDirection(model.stats.rolls[model.stats.rolls.length-2]);
+
+        if(lastDirection==='MISS') {
+            model.stats[pattern].miss++;
+
+            model.stats[pattern].missStreak++;
+            model.stats[pattern].sameStreak = 0;
+            model.stats[pattern].oppositeStreak = 0;
+
+            if(model.stats[pattern].missStreak>model.stats[pattern].highestMiss) {
+                model.stats[pattern].highestMiss = model.stats[pattern].missStreak;
+            }
+        } else if(lastDirection === directionBeforeLast) {
+            model.stats[pattern].same++;
+
+            model.stats[pattern].missStreak = 0;
+            model.stats[pattern].sameStreak++;
+            model.stats[pattern].oppositeStreak = 0;
+
+            if(model.stats[pattern].sameStreak>model.stats[pattern].highestSame) {
+                model.stats[pattern].highestSame = model.stats[pattern].sameStreak;
+            }
+        } else {
+            model.stats[pattern].opposite++;
+
+            model.stats[pattern].missStreak = 0;
+            model.stats[pattern].sameStreak = 0;
+            model.stats[pattern].oppositeStreak++;
+
+            if(model.stats[pattern].oppositeStreak>model.stats[pattern].highestOpposite) {
+                model.stats[pattern].highestOpposite = model.stats[pattern].oppositeStreak;
+            }
+        }
+    },
+
     getRandomInt: function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
@@ -188,7 +361,7 @@ mv = {
 
 
 function mainLoop() {
-    console.log(model.actionArr[model.actionIndex]);
+    //console.log(model.actionArr[model.actionIndex]);
     switch(model.actionArr[model.actionIndex]) {
         case 'init':
             console.log('initializing script...');
@@ -210,7 +383,12 @@ function mainLoop() {
 
         case 'set real bet':
             mv.setBetAmount();
-            mv.setSameDirection();
+            if(model.settings.patternZigZag===true) {
+                mv.setSameDirection();
+            } else {
+                //mv.setSameDirection();
+                mv.setOppositeDirection();
+            }
 
             mv.increaseActionIndex(); // move to next action
         break;
@@ -237,11 +415,25 @@ function mainLoop() {
                 mv.recordRoll();
 
                 if( model.betting ) {
+                    mv.increaseDirectionCount();
                     if( mv.iWin() ) {
+                        lStreakCounter = 0;
+                        wStreakCounter++;
+                        if(wStreak<wStreakCounter) wStreak = wStreakCounter;
+
+                        console.log('won');
+                        mv.increaseWinCount();
                         mv.betClickCurrentReset();
                         mv.setBetting(false);
                     } else {
+                        wStreakCounter = 0;
+                        lStreakCounter++;
+                        if(lStreak<lStreakCounter) lStreak = lStreakCounter;
+
+                        console.log('lost');
+                        mv.increaseLossCount();
                         mv.betClickCurrentIncrease();
+                        mv.setBetting(false);
                     }
                 } else {
                     if( mv.lastRollsZigZag() ) {
@@ -270,10 +462,14 @@ function stop() {
     clearTimeout(timer);
 };
 
+function loop() {
+    mainLoop();
+}
+
 function start() {  // use a one-off timer
     model.actionArr.push('init');
     model.actionIndex = 0;
-    timer = setInterval(mainLoop, 1000);
+    timer = setInterval(loop, 1000);
 };
 
 start();
